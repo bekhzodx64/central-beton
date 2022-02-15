3eb57c7f7ab855aac7448c7fb9e43d27335db8bf
bekhzodx64 <mr.bekzod2018@gmail.com>
2022-02-15 21:56:47 +0500

    Пофиксил баг с modal-window

--- dist/js/modal-window.js
+++ dist/js/modal-window.js
@@ -5,24 +5,30 @@ document.addEventListener('DOMContentLoaded', () => {
 	const modalFormClose = document.querySelector('.modal-form__close');
 	const heroBtn = document.querySelectorAll('.hero-btn');
 	const formBtn = document.querySelector('.form-btn');
+	const body = document.querySelector('body');
 
 	heroBtn.forEach((btn) => {
-		btn.addEventListener('click', () => {
+		btn.addEventListener('click', (e) => {
+			e.preventDefault();
 			modalForm.classList.remove('hidden');
+			body.classList.add('overflow-hidden');
 		});
 	});
 
 	modalFormClose.addEventListener('click', () => {
 		modalForm.classList.add('hidden');
+		body.classList.remove('overflow-hidden');
 	});
 
 	formBtn.addEventListener('click', () => {
 		modalForm.classList.remove('hidden');
+		body.classList.add('overflow-hidden');
 	});
 
 	modalFormWrapper.addEventListener('click', (e) => {
 		if (e.target.classList.contains('modal-form__wrapper')) {
 			modalForm.classList.add('hidden');
+			body.classList.remove('overflow-hidden');
 		}
 	});
 });
